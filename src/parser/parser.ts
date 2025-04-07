674ffa426daef2a4a2ee40cf1affaf600d06b61f
Sam Saccone <samccone@gmail.com>
2025-04-07 10:21:43 -0700

    Clean up markdown usage and bump package

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -17,36 +17,21 @@ import Token from 'markdown-it/lib/token';
 // @ts-ignore
 import attrs from 'markdown-it-attrs';
 // @ts-ignore
-import lazyHeaders from 'markdown-it-lazy-headers';
-// @ts-ignore
-import emoji from 'markdown-it-emoji';
-// @ts-ignore
-import expandTabs from 'markdown-it-expand-tabs';
-// @ts-ignore
 import video from 'markdown-it-video';
 // @ts-ignore
 import customFence from 'markdown-it-fence';
 
-function generatedImage(md: unknown): void {
-  return customFence(md, 'generated_image', {
-    marker: '$',
-    validate: () => true,
-  });
-}
 
 const mdOptions = {
-  html: true,
+  htm: true,
   langPrefix: 'highlight ',
-  linkify: false,
+  linkify: true,
+  typographer: true,
   breaks: false,
 };
 
 const parser = markdownIt(mdOptions)
   .use(attrs)
-  .use(lazyHeaders)
-  .use(emoji, {shortcuts: {}})
-  .use(expandTabs, {tabWidth: 4})
-  .use(generatedImage)
   .use(video, {youtube: {width: 640, height: 390}});
 
 function parseMarkdown(markdown: string): Token[] {
